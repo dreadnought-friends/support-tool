69bed5d2dd2b574ec7b904b558ca33ff3cbfd2eb
Iltar van der Berg <kjarli@gmail.com>
2017-05-20 21:12:07 +0200

    Some more cleanup

--- support-tool.bat
+++ support-tool.bat
@@ -2,7 +2,7 @@
 mode con: cols=68 lines=32
 title Dreadnought Community Support Tool
 
-setlocal ENABLEDELAYEDEXPANSION
+setlocal EnableDelayedExpansion
 
 set version=1.0.0
 set sourceDirPath=C:\DreadnoughtSupport\
@@ -23,7 +23,7 @@ echo     Let the tool run until you see a file on your desktop called
 echo     %targetZipFileName%
 echo:
 echo     Authors:
-echo "      - Anyone#6415 <kjarli@gmail.com>"
+echo      - Anyone#6415
 echo:
 echo     For feedback, please contact us on Discord:
 echo      - https://discord.gg/dreadnought
@@ -40,7 +40,7 @@ echo      - "%userprofile%\AppData\Local\DreadGame\Saved\Logs"
 echo:
 echo   ----------------------------------------------------------------
 echo:
-echo:    Press ENTER to start or close the tool to quit.
+echo:    Press ENTER to start, or close the tool to quit.
 
 pause > nul
 
@@ -53,21 +53,28 @@ echo     Please don't interrupt or quit the tool while waiting.
 
 set supportReadmeFile=%contentDirPath%\SUPPORT-README.txt
 
-echo Files gathered and zipped by the Dreadnought Community Support Tool (%version%) > %supportReadmeFile%
-echo Automatically collected files: >> %supportReadmeFile%
-echo  - /logs/*.log >> %supportReadmeFile%
-echo  - /dxDiag.txt >> %supportReadmeFile%
-echo  - /MSInfo.txt >> %supportReadmeFile%
+:: Readme file included for the Support Employee that this was automatically generated
+echo Files gathered and archived by the Dreadnought Community Support Tool (%version%) >> %supportReadmeFile%
+echo:                                                                                  >> %supportReadmeFile%
+echo Automatically collected information:                                              >> %supportReadmeFile%
+echo  - Dreadnought log files                                                          >> %supportReadmeFile%
+echo  - DxDiag                                                                         >> %supportReadmeFile%
+echo  - MSInfo                                                                         >> %supportReadmeFile%
+echo:                                                                                  >> %supportReadmeFile%
+echo The Dreadnought Community Support Tool can be found at:                           >> %supportReadmeFile%
+echo  - https://github.com/dreadnought-friends/support-tool                            >> %supportReadmeFile%
+echo:                                                                                  >> %supportReadmeFile%
 
 forfiles /P "%userprofile%\AppData\Local\DreadGame\Saved\Logs" /M *.log /C "cmd /c xcopy @PATH %contentDirPath%\logs 2>&1 > nul"
 
-dxdiag /t %contentDirPath%\DxDiag.txt
-Msinfo32 /report %contentDirPath%\MSInfo.txt
+dxdiag /t %contentDirPath%\dxdiag.txt
+Msinfo32 /report %contentDirPath%\msinfo.txt
 
 set tempFilePath=%temp%\FilesToZip.txt
 type nul > %tempFilePath%
 
-FOR /f "delims=*" %%i IN ('dir /b /s /a-d "%sourceDirPath%"') do (
+:: Creating a zip file, not sure what this magic does...
+for /f "delims=*" %%i in ('dir /b /s /a-d  "%sourceDirPath%"') do (
   set filePath=%%i
   set dirPath=%%~dpi
   set dirPath=!dirPath:~0,-1!
@@ -88,7 +95,7 @@ echo     %targetZipFileName% has been created and is located on your
 echo     Desktop. Please include this file as attachment to your
 echo     support ticket or email.
 echo: 
-echo     You can now close the DREADNOUGHT COMMUNITY SUPPORT TOOL!
+echo     You can now close the Dreadnought Community Support Tool!
 echo:
 echo   ----------------------------------------------------------------
 echo:
